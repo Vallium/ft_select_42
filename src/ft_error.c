6dde70cf83fe0f1a5bf9f774781bec1918c7fec9
Vallium <anton.alliot@gmail.com>
2016-11-24 16:24:39 +0100

    Rename and norme for ft_error and winsize methods

--- src/ft_error.c
+++ src/ft_error.c
@@ -1,29 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   win.c                                              :+:      :+:    :+:   */
+/*   ft_error.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aalliot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/11/16 15:42:37 by aalliot           #+#    #+#             */
-/*   Updated: 2016/11/16 15:42:38 by aalliot          ###   ########.fr       */
+/*   Created: 2016/11/16 15:38:55 by aalliot           #+#    #+#             */
+/*   Updated: 2016/11/24 15:15:49 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_select.h"
 
-void	winsize(void)
+void	ft_error(char *err)
 {
-	t_term	*term;
-
-	term = ft_singleton();
-	if (ioctl(STDIN_FILENO, TIOCGWINSZ, (char *)&term->winsize) < 0)
-		ft_error("ft_select: error: Can't get terminal size.");
-	if (term->nb_entries - 2)
-	{
-		term->nb_column = term->winsize.ws_col / (term->longest + 2);
-		term->total_column = (term->nb_entries / term->winsize.ws_row) + 1;
-		if (term->nb_column >= term->total_column)
-			term->padding_left = 0;
-	}
+	reset_term();
+	ft_putendl_fd(err, 2);
+	exit(EXIT_FAILURE);
 }
--- src/winsize.c
+++ src/winsize.c
@@ -1,20 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error.c                                            :+:      :+:    :+:   */
+/*   winsize.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aalliot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/11/16 15:38:55 by aalliot           #+#    #+#             */
-/*   Updated: 2016/11/24 15:15:49 by aalliot          ###   ########.fr       */
+/*   Created: 2016/11/16 15:42:37 by aalliot           #+#    #+#             */
+/*   Updated: 2016/11/16 15:42:38 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_select.h"
 
-void	ft_error(char *err)
+void	winsize(void)
 {
-	reset_term();
-	ft_putendl_fd(err, 2);
-	exit(EXIT_FAILURE);
+	t_term	*term;
+
+	term = ft_singleton();
+	if (ioctl(STDIN_FILENO, TIOCGWINSZ, (char *)&term->winsize) < 0)
+		ft_error("ft_select: error: Can't get terminal size.");
+	if (term->nb_entries - 2)
+	{
+		term->nb_column = term->winsize.ws_col / (term->longest + 2);
+		term->total_column = (term->nb_entries / term->winsize.ws_row) + 1;
+		if (term->nb_column >= term->total_column)
+			term->padding_left = 0;
+	}
 }
