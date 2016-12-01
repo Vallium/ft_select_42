30cd9f52f438f855eec66a24021d983bb975f723
Vallium <anton.alliot@gmail.com>
2016-12-01 16:51:45 +0100

    add signals abort and kill handling

--- src/signals.c
+++ src/signals.c
@@ -17,6 +17,8 @@ void	init_signals(void)
 {
 	signal(SIGINT, sig_exit);
 	signal(SIGQUIT, sig_exit);
+	signal(SIGABRT, sig_exit);
+	signal(SIGKILL, sig_exit);
 	signal(SIGWINCH, sig_winch);
 	signal(SIGTSTP, sig_tstp);
 	signal(SIGCONT, sig_cont);
