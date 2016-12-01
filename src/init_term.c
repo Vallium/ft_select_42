37372c42fadbd0af39d7867e74ce70a47fbc9d7a
Vallium <anton.alliot@gmail.com>
2016-12-01 13:00:43 +0100

    Norme init_term()

--- src/init_term.c
+++ src/init_term.c
@@ -13,11 +13,31 @@
 #include "ft_select.h"
 #include "stdio.h"
 
-int		init_term(void)
+static int		init_term_norme(void)
+{
+	t_term	*term;
+	char	*res;
+
+	term = ft_singleton();
+	if (tcsetattr(0, TCSADRAIN, &term->termios) == -1)
+		ft_error("ft_select: TCSADRAIN error");
+	if ((term->cap[MR] = tgetstr("mr", NULL)) == NULL ||
+		(term->cap[ME] = tgetstr("me", NULL)) == NULL ||
+		(term->cap[US] = tgetstr("us", NULL)) == NULL ||
+		(term->cap[UE] = tgetstr("ue", NULL)) == NULL ||
+		(term->cap[CM] = tgetstr("cm", NULL)) == NULL ||
+		(term->cap[CL] = tgetstr("cl", NULL)) == NULL)
+		ft_error("ft_select: tgetstr error");
+	if ((res = tgetstr("vi", NULL)) == NULL)
+		ft_error("tgetstr vi error\n");
+	tputs(res, 0, ft_my_outc);
+	return (1);
+}
+
+int				init_term(void)
 {
 	t_term	*term;
 	char	*term_name;
-	char	*res;
 
 	term = ft_singleton();
 	term->fd = open(ttyname(STDIN_FILENO), O_WRONLY);
@@ -34,27 +54,8 @@ int		init_term(void)
 	term->termios.c_lflag &= ~(ICANON | ECHO);
 	term->termios.c_cc[VMIN] = 1;
 	term->termios.c_cc[VTIME] = 0;
-	if (tcsetattr(0, TCSADRAIN, &term->termios) == -1)
-	{
-		ft_putstr_fd("TCSADRAIN error\n", 2);
+	if (!init_term_norme())
 		return (-1);
-	}
-	if ((term->cap[MR] = tgetstr("mr", NULL)) == NULL ||
-		(term->cap[ME] = tgetstr("me", NULL)) == NULL ||
-		(term->cap[US] = tgetstr("us", NULL)) == NULL ||
-		(term->cap[UE] = tgetstr("ue", NULL)) == NULL ||
-		(term->cap[CM] = tgetstr("cm", NULL)) == NULL ||
-		(term->cap[CL] = tgetstr("cl", NULL)) == NULL)
-	{
-		ft_putstr_fd("tgetstr error\n", 2);
-		return (-1);
-	}
-	if ((res = tgetstr("vi", NULL)) == NULL)
-	{
-		ft_putstr_fd("tgetstr vi error\n", 2);
-		return (-1);
-	}
-	tputs(res, 0, ft_my_outc);
 	term->padding_left = 0;
 	winsize();
 	return (0);
