d0a5b77d8afed8dc8d32f3426786e6fb09112e4f
Vallium <anton.alliot@gmail.com>
2016-12-01 13:11:23 +0100

    init_entries() norme

--- src/init_entries.c
+++ src/init_entries.c
@@ -12,39 +12,44 @@
 
 #include "ft_select.h"
 
-void	init_entries(int ac, char *av[])
+static void		set_entry(char *av, int i)
+{
+	t_entry	entry;
+	t_term	*term;
+	t_lstd	*tmp;
+	int		len;
+
+	term = ft_singleton();
+	len = ft_strlen(av);
+	entry.name = ft_strdup(av);
+	entry.selected = 0;
+	entry.id = i - 1;
+	if (!(tmp = ft_lstdnew(&entry, sizeof(t_entry))))
+		ft_error("ft_select: entries failed");
+	ft_lstdadd(&(term->entries), tmp);
+	if (len > term->longest)
+		term->longest = len;
+}
+
+void			init_entries(int ac, char *av[])
 {
 	t_term	*term;
 	int		i;
-	t_lstd	*tmp;
-	t_entry	entry;
-	int		len;
 
 	term = ft_singleton();
 	term->longest = 0;
 	term->nb_entries = ac - 1;
 	term->entries = NULL;
+	term->padding_left = 0;
 	i = 1;
 	while (i < ac)
 	{
-		if (!ft_strcmp(av[i], ""))
-		{
+		if (ft_strcmp(av[i], ""))
+			set_entry(av[i], i);
+		else
 			term->nb_entries--;
-			i++;
-			continue;
-		}
-		len = ft_strlen(av[i]);
-		entry.name = ft_strdup(av[i]);
-		entry.selected = 0;
-		entry.id = i - 1;
-		if (!(tmp = ft_lstdnew(&entry, sizeof(t_entry))))
-			ft_error("ft_select: entries failed");
-		ft_lstdadd(&(term->entries), tmp);
-		if (len > term->longest)
-			term->longest = len;
 		i++;
 	}
 	term->hover = term->entries;
-	term->padding_left = 0;
 	winsize();
 }
